3761d5aa1e685db9b77b5b422fad14a94e81bb6e
jinglemansweep <jinglemansweep@f0c030cf-4955-0410-9e82-57b5e7a38422>
2008-08-25 15:12:51 +0000

    Added GPL licence headers

--- lib/squeezecenter/SqueezeCenter.py
+++ lib/squeezecenter/SqueezeCenter.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 
+"""
+PySqueezeCenter: Python Wrapper for Logitech SqueezeCenter CLI (Telnet) Interface
+Copyright (C) 2008 JingleManSweep <jinglemansweep [at] gmail [dot] com>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+
 import sys
 import telnetlib
 import urllib
@@ -44,7 +63,7 @@ class SqueezeCenter:
 
         def get_players(self):
             """
-            Scan Players
+            Get Players
             """
             self.players = []
             player_count = self.get_player_count()
@@ -54,21 +73,32 @@ class SqueezeCenter:
             return self.players
 
         def get_player(self, id=None):
+            """
+            Get Player
+            """
             if id:
                 for player in self.players:
                     if str(player._properties["id"]).lower() == str(id).lower():
                         return player
-                
 
         def get_version(self):
+            """
+            Get Version
+            """
             self.version = self._request("version ?")
             return self.version
         
         def get_player_count(self):
+            """
+            Get Number Of Players
+            """
             self.player_count = self._request("player count ?")
             return int(self.player_count)
         
         def connect(self):
+            """
+            Connect
+            """
             self._connect()
             self._login()
             
