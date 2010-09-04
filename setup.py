7b8d604aac553de02c81234af8dd55d4f2f36f5a
jinglemansweep@gmail.com <jinglemansweep@gmail.com@f0c030cf-4955-0410-9e82-57b5e7a38422>
2010-09-04 21:28:48 +0000

    Fixed typo in valid command list of CLI tool.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name="pysqueezecenter",
-	version="0.91",
+	version="0.92",
 	description="PySqueezeCenter",
 	author="Louis King",
 	author_email="jinglemansweep@gmail.com",
