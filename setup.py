93f82b330876c1246267a9d7ca67d20db5022d68
jinglemansweep <jinglemansweep@f0c030cf-4955-0410-9e82-57b5e7a38422>
2011-08-14 12:07:33 +0000

    Updated documentation and version number.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name="pysqueezecenter",
-	version="0.94",
+	version="0.95",
 	description="PySqueezeCenter",
 	author="Louis King",
 	author_email="jinglemansweep@gmail.com",
